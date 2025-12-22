385335937edceaa171dd261cb6b455892110fe12
vedavyas <vyasatadkod90@gmail.com>
2025-12-22 16:55:27 +0530

    Sync workspace changes  automated commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
+import { loadRemoteModule } from '@angular-architects/module-federation';
+import { provideHttpClient } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadChildren: () => loadRemoteModule({
+            type: 'module',
+            remoteEntry: 'http://localhost:8081/remoteEntry.js',
+            exposedModule: './routes'
+        }).then(m => m.routes)
+    },
+    {
+        path: 'standalone',
+        loadComponent: () => loadRemoteModule({
+            type: 'module',
+            remoteEntry: 'http://localhost:8081/remoteEntry.js',
+            exposedModule: './StandaloneComponent'
+        }).then(m => m.StandaloneComponent),
+        providers: [
+            provideHttpClient(),
+            importProvidersFrom([
+                // Other modules if needed like tranlasteModule.forRoot(...)
+            ])
+        ]
+    }
+];
