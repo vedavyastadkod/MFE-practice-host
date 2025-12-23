6b2e3deaa05da38ca1ad16161e0e5b3d603f325c
vedavyas <vyasatadkod90@gmail.com>
2025-12-23 20:15:45 +0530

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
 const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
 
-module.exports = withModuleFederationPlugin({
-  shared: {
-    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+module.exports = {
+  output: {
+    uniqueName: 'my-host',
+    publicPath: 'auto',
   },
+  Plugins: [
+    new withModuleFederationPlugin({
+      library: { type: 'module'},
+      shared: {
+        ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+      },
+    })
+  ]
 
-});
+};
